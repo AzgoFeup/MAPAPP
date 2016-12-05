c5a7a78a31b36ec21b065e9499cfc0c5c64f5c3a
catarinacruz <catarina.m.c.2401@gmail.com>
2016-12-05 20:06:58 +0000

    (PROGRESSO) Atividade ao carregar nos botões das settings, mas não completo

--- app/src/main/java/com/azgo/mapapp/ButtonActivity.java
+++ app/src/main/java/com/azgo/mapapp/ButtonActivity.java
@@ -1,22 +1,55 @@
 package com.azgo.mapapp;
 
-import android.widget.CompoundButton;
-import android.widget.ToggleButton;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.wallet.fragment.SupportWalletFragment;
+
 
 /**
  * Created by catar on 05/12/2016.
  */
 
-public class Switch extends CompoundButton {
+public class ButtonActivity extends Button implements View.OnClickListener {
 
-    ToggleButton toggle = (ToggleButton) findViewById(R.id.togglebutton);
-    toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            if (isChecked) {
-                // The toggle is enabled
-            } else {
-                // The toggle is disabled
-            }
-        }
-    });
+    private static final int[] mStates = { R.attr.settings_on, R.attr.settings_off };
+    private int mStateIndex = 0; // first state is "on"
+
+    private SupportWalletFragment.OnStateChangedListener mListener;
+    public ButtonActivity(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        changeState();
+    }
+
+    public void changeState() {
+        mStateIndex = (mStateIndex+1) % mStates.length;
+
+        // notify listener
+        if(mListener != null) mListener.onStateChanged(mStates[mStateIndex]);
+    }
+
+    @Override
+    protected int[] onCreateDrawableState(int extraSpace) {
+
+        final int[] drawableState = super.onCreateDrawableState(extraSpace+1);
+
+        int [] state = { mStates[mStateIndex] };
+
+        mergeDrawableStates(drawableState, state);
+
+        return drawableState;
+    }
+
+    public void setOnStateChangedListener(SupportWalletFragment.OnStateChangedListener l) {
+        this.mListener = l;
+    }
 }
+
