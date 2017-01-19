486d395737d327e2518bfcf9c3f26871a4eaf307
José Valverde <delfimvalverde@gmail.com>
2017-01-19 16:14:10 +0000

    Coommunication is working

--- app/src/main/java/com/azgo/mapapp/MainActivity.java
+++ app/src/main/java/com/azgo/mapapp/MainActivity.java
@@ -115,6 +115,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     Toolbar toolbar = null;
     DrawerLayout drawer = null;
     ActionBarDrawerToggle toggle = null;
+    Button testButton = null;
 
     String roomMeet,emailMeet;
 
@@ -371,7 +372,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
 
 
-        final Button testButton = (Button) findViewById(R.id.startActivityButton);
+        testButton = (Button) findViewById(R.id.startActivityButton);
         testButton.setTag(1);
         testButton.setBackgroundResource(R.drawable.navigate);
         testButton.setOnClickListener(new View.OnClickListener() {
@@ -496,7 +497,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         Graph.Node searchNode = null;
         for (Graph.Node no : nodes) {
-            if (no.getLabel().equals(location)) {
+            if (no.getLabel().equals(location.toUpperCase())) {
                 searchNode = no;
                 break;
             }
@@ -1097,6 +1098,44 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
+    public void navigationMeet(String room) {
+        location = room;
+        PolylineOptions linePath = new PolylineOptions();
+        //mLastLocation = location;
+        //Get map on navigation mode
+        LatLng latLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+        // Construct a CameraPosition focusing on current position and animate the camera to that position.
+        //change camera view on current user's location to start navigation
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(latLng)      // Sets the center of the map to Mountain View
+                .zoom(21)                   // Sets the zoom
+                .tilt(60)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        Graph.Node searchNode = null;
+        for (Graph.Node no : nodes) {
+            if (no.getLabel().equals(location.toUpperCase())) {
+                searchNode = no;
+                break;
+            }
+        }
+        startNavigationTo(searchNode, mCurrentLocation);
+
+
+        //Intent i = new Intent(this, Navigation.class);
+        //i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //i.putExtra("location", locationMessage);
+        //send String to navigate to room number
+       /* if(location!=null && !location.isEmpty()){
+            Toast.makeText(this, "Starting navigation to "+location, Toast.LENGTH_LONG).show();
+        }*/
+        //startActivity(i);
+        //finish();
+        //location.equals(null);
+
+    }
+
     /**
      * Api time method to help communication
      *
@@ -1104,6 +1143,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
      */
     public void navigateToRoom(String room) {
 
+
+        final int status = (Integer) testButton.getTag();
+        if (status == 1) {
+            navigationMeet(room);
+            testButton.setBackgroundResource(R.drawable.cancelnavigation);
+            navigation_on = 1;
+            testButton.setTag(0);
+        } else {
+            testButton.setBackgroundResource(R.drawable.navigate);
+            stopNavigation(mCurrentLocation);
+            testButton.setTag(1);
+            navigation_on = 0;
+        }
+
+        /*
         Graph.Node searchNode = null;
         for (Graph.Node no : nodes) {
             if (no.getLabel().equals(room)) {
@@ -1111,9 +1165,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
             }
         }
-        if (searchNode == null)
+        if (searchNode != null)
             //TODO: something
             startNavigationTo(searchNode, mCurrentLocation);
+        */
     }
 
     //Communication
@@ -1532,16 +1587,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             synchronized (lockfriends) {
 
 
-                //String[] items = values[0].split("\\$"); //values[0] está a mensagem toda do server
+                String[] items = values[0].split("\\$"); //values[0] está a mensagem toda do server
 
                 //ToDebug:
 
 
-                String[] items = new String[6];
+                /*String[] items = new String[6];
                 for (int i = 0; i < 6; i++) {
                     items[i] = "ola" + i + "#ole" + i + "#oli" + i;
                     Log.d("FrindsAsinc", items[i]);
-                }
+                }*/
 
 
                 int i = 0;
